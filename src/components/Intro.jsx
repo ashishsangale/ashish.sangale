23633e701fd42bc33d5551a5b177927c346a3e4d
ashishsangale <aashishsangale07@gmail.com>
2024-05-06 00:05:48 +0530

    intro page typed animation

--- src/components/Intro.jsx
+++ src/components/Intro.jsx
@@ -1,24 +1,34 @@
-import React from 'react'
 import img from './Images/IMG_3495.jpg'
 import { Container, Row, Col } from 'react-bootstrap'
+import { ReactTyped } from "react-typed"
 
 function Intro() {
   return (
-    <Container fluid>
+    <section  id='hero' className='d-flex flex-column justify-content-center'>
+      <Container>
         <Row>
-            <Col lg = {6} className='item-center'>1</Col>
+            <Col lg = {6} className='item-center'>
+              <h1 className='h1 fixed-top'>Ashish Sangale</h1>
+              <p> I am a&nbsp; </p>
+              <span
+                className="typed"
+                data-typed-items="Developer, Tabla Player, Gamer"
+              ></span>
+              <ReactTyped strings={["Backend Developer", "Sports Entusiast"]} typeSpeed={40} loop showCursor/>
+            </Col>
             <Col lg = {6} style={{
                 display: 'flex',
                 alignItems: "center",
                 justifyContent: "center",
                 padding: "15px"
             }}>
-                <img src={img} height='100%' width='65%' style={{
+                <img src={img} height ='100%' width='65%' style={{
                 borderRadius: "30px",
               }}></img>
             </Col>
         </Row>
-    </Container>
+      </Container>
+    </section>
   )
 }
 
